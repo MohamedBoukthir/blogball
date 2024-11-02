a8dff92b73e7abbbc8765e9a6c0d7fd504db69da
MohamedBoukthir <medboukthir7@gmail.com>
2024-11-02 22:10:52 +0100

    first commit

---
+++ blogball/src/main/java/com/mohamed/blogball/BlogballApplication.java
@@ -0,0 +1,13 @@
+package com.mohamed.blogball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogballApplication.class, args);
+	}
+
+}
---
+++ blogball/src/test/java/com/mohamed/blogball/BlogballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohamed.blogball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
