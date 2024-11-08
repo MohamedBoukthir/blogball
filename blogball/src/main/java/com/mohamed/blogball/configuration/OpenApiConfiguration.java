7bc6ae7a89aaf9a6120e51cf29654277fc230a43
MohamedBoukthir <medboukthir7@gmail.com>
2024-11-08 22:43:12 +0100

    feat(backend): add necessary dependencies and configure OpenAPI for documentations

---
+++ blogball/src/main/java/com/mohamed/blogball/configuration/OpenApiConfiguration.java
@@ -0,0 +1,36 @@
+package com.mohamed.blogball.configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Mohamed Boukthir",
+                        email = "medboukthir7@gmail.com",
+                        url = "https://github.com/MohamedBoukthir"
+                ),
+                description = "Open API docs for an blog website.",
+                title = "BlogBall API",
+                version = "1.0.0"
+        ),
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "Cooke authentication",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.COOKIE
+)
+public class OpenApiConfiguration {}
