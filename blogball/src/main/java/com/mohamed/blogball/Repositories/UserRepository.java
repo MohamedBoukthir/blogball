3f4110befaa43c3525492fa8929a844586c45699
MohamedBoukthir <medboukthir7@gmail.com>
2024-11-09 14:58:36 +0100

    conf(backend): beans configuration and implement the UserDetailsService

---
+++ blogball/src/main/java/com/mohamed/blogball/Repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.mohamed.blogball.Repositories;
+
+import com.mohamed.blogball.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
+
+}
