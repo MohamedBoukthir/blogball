d77791f355a79fcf2c7070f8d2613147019745d4
MohamedBoukthir <medboukthir7@gmail.com>
2024-11-24 15:29:58 +0100

    add(frontend): navbar for simple user account

--- blogball-ui/src/app/layout/header/header.component.ts
+++ blogball-ui/src/app/layout/header/header.component.ts
@@ -1,9 +1,44 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {StorageService} from "../../authentication/storage.service";
+import {AuthenticationService} from "../../authentication/authentication.service";
+import {NavigationEnd, Router} from "@angular/router";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+
+  isUserLoggedIn : boolean = false;
+  isAdminLoggedIn : boolean = false;
+
+
+  constructor(
+    private authenticationService : AuthenticationService,
+    private storageService : StorageService,
+    private router : Router
+  ) {}
+
+  ngOnInit(): void {
+        this.updateLoginState();
+        this.router.events.subscribe((event) => {
+          if (event instanceof NavigationEnd) {
+            this.updateLoginState();
+          }
+        });
+    }
+
+
+  // Updates the login state by checking with the StorageService.
+  private updateLoginState(): void {
+    this.isAdminLoggedIn = this.storageService.isAdminLoggedIn();
+    this.isUserLoggedIn = this.storageService.isUserLoggedIn();
+  }
+
+  logOut() {
+    this.authenticationService.logOut();
+    this.router.navigateByUrl('/sign-in')
+  }
+
 }
